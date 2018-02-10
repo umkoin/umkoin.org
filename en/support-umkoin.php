69fc0bc90c8a4a2b7e76414bb8bc1f2426870877
Igor Gunia <vmta@yahoo.com>
2018-02-11 00:44:27 +0200

    Support Umkoin page updated.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ en/support-umkoin.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Support Umkoin - Umkoin</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Support Umkoin</h1>
+    <p class="summarytxt">Umkoin is a protocol that was born from a small community and has grown fast since. There are a lot of things you can do to help Umkoin to spread and improve over time.</p>
+
+    <h2 id="use"><img class="titleicon" src="/img/icons/ico_multi.svg" alt="Icon" />Using Umkoin</h2>
+    <p><a href="/en/getting-started.php">Using Umkoin</a> is the first thing you can do to support Umkoin. There are probably many cases where it can make your life easier. You can accept payments and make purchases with Umkoin.</p>
+
+    <h2 id="node"><img class="titleicon" src="/img/icons/ico_network.svg" alt="Icon" />Be the network</h2>
+    <p>If you have a good Internet connection, you can strengthen the Umkoin network by keeping <a href="/en/full-node.php">full node software</a> running on your computer or server with port 6333 open. Full nodes are securing and relaying all transactions.</p>
+
+    <h2 id="mining"><img class="titleicon" src="/img/icons/ico_mining.svg" alt="Icon" />Mining</h2>
+    <p>You can start <a href="http://pool.umkoin.org:8080/" target="_blank">mining umkoins</a> to help processing transactions. In order to protect the network, you should join smaller mining pools and prefer decentralized pools with getblocktemplate (GBT) support.</p>
+
+    <h2 id="translate"><img class="titleicon" src="/img/icons/ico_translate.svg" alt="Icon" />Translate</h2>
+    <p>You can help to increase Umkoin availability by translating or improving translations inside important parts of the Umkoin ecosystem. Just pick a project you would like to help.</p>
+
+    <h2 id="develop"><img class="titleicon" src="/img/icons/ico_conf.svg" alt="Icon" />Development</h2>
+    <p>Umkoin is free software. So if you are a developer, you can use your super-powers to do good and <a href="/en/development.php">improve Umkoin</a>. Or you can build amazing new services or software that can use Umkoin.</p>
+
+    <h2 id="donation"><img class="titleicon" src="/img/icons/ico_donate.svg" alt="Icon" />Donation</h2>
+    <p>The easiest way to help is to donate a few umkoins. Or you can help directly fund any project related to Umkoin that you believe will be helpful in the future.</p>
+
+    <h2 id="nonprofit"><img class="titleicon" src="/img/icons/ico_law.svg" alt="Icon" />Organizations</h2>
+    <p>Many non-profit organizations are dedicated to protecting and promoting Umkoin. You can help these groups by joining them and taking part in their projects, discussions and events.</p>
+
+    <h2 id="spread"><img class="titleicon" src="/img/icons/ico_spread.svg" alt="Icon" />Spread</h2>
+    <p>Speak about Umkoin to interested people. Write about it on your blog. Tell your favorite shops you would like to pay with Umkoin. Help to keep merchant directories up to date. Or be creative and make yourself a nice Umkoin T-shirt.</p>
+
+    <h2 id="wiki"><img class="titleicon" src="/img/icons/ico_wiki.svg" alt="Icon" />Documentation</h2>
+    <p><a href="https://github.com/umkoin/umkoin.org#how-to-participate">Umkoin.org</a> provides useful documentation and we are constantly improving the information it contains. You can help to improve this resource and keep it up to date.</p>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+<script type="text/javascript">
+  fallbackSVG();
+  addAnchorLinks();
+  trackOutgoingLinks();
+</script>
+
+
+</body>
+</html>
---
+++ uk/support-umkoin.php
@@ -0,0 +1,84 @@
+<!DOCTYPE HTML>
+<html lang="uk">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Підтримка Умкойн - Умкойн</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+<link rel="stylesheet" href="/css/sans.css">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Підтримка Умкойн</h1>
+    <p class="summarytxt">Умкойн - це протокол, що був започаткований маленькою спільнотою і з тих пір швидко зростає по висхідній. Існує безліч речей, які ви можете зробити, щоб сприяти поширенню та поступовому покращенню Умкойн.</p>
+
+    <h2 id="use"><img class="titleicon" src="/img/icons/ico_multi.svg" alt="Icon" />Використання Умкойн</h2>
+    <p>Перше, що ви можете зробити, щоб допомогти Умкойн, це почати <a href="/uk/getting-started.php">використовувати Умкойн</a>. Існує багато шляхів, завдяки яким він може полегшити ваше життя. Ви можете отримувати платежі та здійснювати покупки з Умкойн.</p>
+
+    <h2 id="node"><img class="titleicon" src="/img/icons/ico_network.svg" alt="Icon" />Станьте частиною мережі</h2>
+    <p>Якщо у вас високошвидкісне підключення до Інтернет, ви можете зміцнити умкойн-мережу шляхом установки <a href="/en/full-node.php">повноцінного умкойн-вузла</a> та запустивши його на вашому комп'ютері чи сервері з відкритим портом 6333. Повноцінні вузли забезпечують безпеку та пересилання усіх транзакцій.</p>
+
+    <h2 id="mining"><img class="titleicon" src="/img/icons/ico_mining.svg" alt="Icon" />Видобуток</h2>
+    <p>Ви можете почати <a href="http://pool.umkoin.org:8080/" target="_blank">видобуток умкойнів</a>, щоб допомогти опрацьовувати транзакції. Для захисту мережі, вам варто приєднатись до невеликих видобувних пулів, обрати децентралізований пул з підтримкою getblocktemplate (GBT).</p>
+
+    <h2 id="translate"><img class="titleicon" src="/img/icons/ico_translate.svg" alt="Icon" />Переклад</h2>
+    <p>Ви можете допомогти підвищити рівень доступності Умкойн перекладаючи чи покращуючи переклади всередині важливих частин екосистеми Умкойн. Просто оберіть проект, у якому ви бажаєте взяти участь.</p>
+
+    <h2 id="develop"><img class="titleicon" src="/img/icons/ico_conf.svg" alt="Icon" />Розробка</h2>
+    <p>Умкойн - це програмне забезпечення з відкритим початковим кодом. Тому, якщо ви розробник, ви можете використати свої суперсили в ім'я добрих сил та <a href="/uk/development.php">покращення Умкойн</a>. Або ви можете створити нові приголомшливі сервіси чи програмне забезпечення, яким може скористатись Умкойн.</p>
+
+    <h2 id="donation"><img class="titleicon" src="/img/icons/ico_donate.svg" alt="Icon" />Пожертвування</h2>
+    <p>Найлегший шлях допомогти нам - це пожертвувати декілька умкойнів. Або ви можете безпосередньо профінансувати проект, пов'язаний із умкойн, який на вашу думку буде доцільним у майбутньому.</p>
+
+    <h2 id="nonprofit"><img class="titleicon" src="/img/icons/ico_law.svg" alt="Icon" />Організації</h2>
+    <p>Багато неприбуткових організацій працюють задля захисту та просування умкойн. Ви можете допомогти їм приєднавшись до них та взявши участь у їх проектах, дискусіях та подіях.</p>
+
+    <h2 id="spread"><img class="titleicon" src="/img/icons/ico_spread.svg" alt="Icon" />Поширення</h2>
+    <p>Розкажіть про умкойн тим, хто цікавиться ним. Напишіть про нього у своєму блозі. Повідомте своїм улюбленим магазинам, що ви хотіли б розраховуватись умкойнами. Допоможіть підтримувати <a href="/en/spend-umkoin.php">каталоги компаній</a> актуальними та оновленими. Чи просто проявіть креативність та зробіть собі футболку з гарним логотипом Умкойн.</p>
+
+    <h2 id="wiki"><img class="titleicon" src="/img/icons/ico_wiki.svg" alt="Icon" />Документація</h2>
+    <p><a href="https://github.com/umkoin/umkoin.org#how-to-participate">Umkoin.org</a> надає корисну документацію та постійно оновлює свою інформаційну базу. Ви можете допомогти покращити цей, або інші подібні ресурси, та підтримувати їх актуальність.</p>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+<script type="text/javascript">
+  fallbackSVG();
+  addAnchorLinks();
+  trackOutgoingLinks();
+</script>
+
+
+</body>
+</html>
