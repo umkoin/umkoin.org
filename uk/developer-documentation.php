a7d7ec505a2286ebb1f8897fc234f4cd70437c46
Igor Gunia <vmta@yahoo.com>
2018-02-11 23:58:10 +0200

    Developer documentation page updated.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ uk/developer-documentation.php
@@ -0,0 +1,146 @@
+<!DOCTYPE HTML>
+<html lang="uk">
+
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Документація Розробника - Умкойн</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+<link rel="stylesheet" href="/css/sans.css">
+
+<link rel="stylesheet" href="/css/jquery-ui.min.css">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs">
+    <a href="/uk/">Умкойн</a>
+      &gt;
+      Документація Розробника
+  </div>
+
+  <div id="content" class="content">
+
+    <h1>Документація Розробника</h1>
+    <p class="summary">Знайдіть корисні ресурси, довідники та довідкові матеріали для розробників</p>
+
+    <div class="docreference">
+      <a href="/uk/developer-guide.php"><span class="fa fa-info-circle fa-2x"></span><span>Керівництво</span></a>
+      <a href="/uk/developer-reference.php"><span class="fa fa-book fa-2x"></span><span>Довідка</span></a>
+      <a href="/uk/developer-examples.php"><span class="fa fa-code fa-2x"></span><span>Приклади</span></a>
+      <a href="/uk/developer-glossary.php"><span class="fa fa-font fa-2x"></span><span>Глосарій</span></a>
+    </div>
+
+    <div class="resources">
+      <div>
+        <div>
+          <h2 id="block_chain"><span class="fa fa-cube fa-lg"></span> Блокчейн</h2>
+          <p><a href="/uk/developer-guide.php#block-chain">Блокчейн - Керівництво</a></p>
+          <p><a href="/uk/developer-reference.php#block-chain">Блокчейн - Довідка</a></p>
+        </div>
+        <div>
+          <h2 id="transactions"><span class="fa fa-exchange fa-lg"></span> Транзакції</h2>
+          <p><a href="/uk/developer-guide#transactions.php">Транзакції - Керівництво</a></p>
+          <p><a href="/uk/developer-reference#transactions.php">Транзакції - Довідка</a></p>
+          <p><a href="/uk/developer-examples#transactions.php">Транзакції - Приклади</a></p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h2 id="contracts"><span class="fa fa-sitemap fa-lg fa-rotate-270"></span> Контракти</h2>
+          <p><a href="/uk/developer-guide.php#contracts">Контракти - Керівництво</a></p>
+        </div>
+        <div>
+          <h2 id="wallets"><span class="fa fa-btc fa-lg"></span> Гаманці</h2>
+          <p><a href="/uk/developer-guide.php#wallets">Гаманці - Керівництво</a></p>
+          <p><a href="https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki"><span class="fa fa-external-link"></span> HD Wallets</a> - BIP32 (English)</p>
+          <p><a href="https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki"><span class="fa fa-external-link"></span> Mnemonic Code</a> - BIP39 (English)</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h2 id="payment-processing"><span class="fa fa-cart-plus fa-lg"></span> Опрацьовування платежів</h2>
+          <p><a href="/uk/developer-guide.php#payment-processing">Опрацьовування платежів - Керівництво</a></p>
+          <p><a href="/uk/developer-examples.php#payment-processing">Опрацьовування платежів - Приклади</a></p>
+          <p><a href="https://github.com/bitcoin/bips/blob/master/bip-0070.mediawiki"><span class="fa fa-external-link"></span> Payment Protocol</a> - BIP70 (English)</p>
+        </div>
+        <div>
+          <h2 id="operating_modes"><span class="fa fa-cogs fa-lg"></span> Оперування Режимами</h2>
+          <p><a href="/uk/developer-guide.php#operating-modes">Оперування Режимами - Керівництво</a></p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h2 id="p2p-network"><span class="fa fa-share-alt fa-lg"></span> P2P Мережа</h2>
+          <p><a href="/uk/developer-guide.php#p2p-network">P2P Мережа - Керівництво</a></p>
+          <p><a href="/uk/developer-reference.php#p2p-network">P2P Мережа - Довідка</a></p>
+          <p><a href="/uk/developer-examples.php#p2p-network">P2P Мережа - Приклади</a></p>
+          <p><a href="https://en.bitcoin.it/wiki/Protocol_specification"><span class="fa fa-external-link"></span> Full Protocol Specification</a> - Wiki (English)</p>
+        </div>
+        <div>
+          <h2 id="mining"><span class="fa fa-puzzle-piece fa-lg"></span> </h2>
+          <p><a href="/uk/developer-guide.php#mining"></a></p>
+          <p><a href="https://github.com/bitcoin/bips/blob/master/bip-0022.mediawiki"><span class="fa fa-external-link"></span> getblocktemplate Fundamentals</a> - BIP22 (English)</p>
+          <p><a href="https://github.com/bitcoin/bips/blob/master/bip-0023.mediawiki"><span class="fa fa-external-link"></span> getblocktemplate Pooled Mining</a> - BIP23 (English)</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="resourcesmore">
+      <div>
+        <h2 id="additional-resources"><span class="fa fa-link fa-lg"></span> Додаткові матеріали</h2>
+        <p><a href="/uk/umkoin-paper.php"><span class="fa fa-external-link"></span> Bitcoin: A Peer-to-Peer Electronic Cash System</a> - Satoshi Nakamoto (English)</p>
+        <p><a href="https://github.com/bitcoin/bips#readme"><span class="fa fa-external-link"></span> Bitcoin Improvement Proposals</a> - GitHub (English)</p>
+        <p><a href="https://github.com/minium/Bitcoin-Spec"><span class="fa fa-external-link"></span> Bitcoin Developer Reference (working paper)</a> - Krzysztof Okupski (English)</p>
+        <p><a href="https://bitcoinj.github.io/#documentation"><span class="fa fa-external-link"></span> Bitcoinj Developer Documentation</a> - bitcoinj.org (English)</p>
+        <p><a href="https://programmingblockchain.gitbooks.io/programmingblockchain/content/"><span class="fa fa-external-link"></span> The C# Bitcoin book (NBitcoin Developer Documentation)</a> - Nicolas Dorier (English)</p>
+        <p><a href="https://en.bitcoin.it/wiki/Category:Technical"><span class="fa fa-external-link"></span> Technical Pages</a> - Wiki (English)</p>
+      </div>
+    </div>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+<script type="text/javascript">
+  fallbackSVG();
+  addAnchorLinks();
+  trackOutgoingLinks();
+</script>
+
+
+<script src="/js/jquery/jquery-1.11.2.min.js"></script>
+<script src="/js/jquery/jquery-ui.min.js"></script>
+<script src="/js/devsearch.js"></script>
+
+
+</body>
+</html>
