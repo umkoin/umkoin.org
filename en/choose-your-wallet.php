50ca7ea4d04aa263aa837af56b727a2fa35869d1
Igor Gunia <vmta@yahoo.com>
2018-02-12 12:28:30 +0200

    Choose your wallet page updated.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ en/choose-your-wallet.php
@@ -0,0 +1,64 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Choose your wallet - Umkoin</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Choose your Umkoin wallet</h1>
+    <p class="summary">Find your wallet and start making payments with merchants and users.</p>
+
+    <div class="walletsdisclaimer">
+      <h2 id="educate-yourself">
+        <img src="/img/icons/ico_warning.svg" class="warningicon" alt="warning">
+        Take time to educate yourself
+      </h2>
+      <p>Umkoin is different from what you know and use every day. Before you start using Umkoin for any serious transaction, be sure to read <a href="/en/you-need-to-know.php"><b>what you need to know</b></a> and take appropriate steps to <a href="/en/secure-your-wallet.php"><b>secure your wallet</b></a>. Always remember that it is your responsibility to choose your wallet carefully and adopt good practices in order to protect your money.</p>
+    </div>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+<script type="text/javascript">
+  fallbackSVG();
+  addAnchorLinks();
+  trackOutgoingLinks();
+</script>
+
+
+</body>
+</html>
---
+++ uk/choose-your-wallet.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML>
+<html lang="uk">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Оберіть свій умкойн-гаманець - Умкойн</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+<link rel="stylesheet" href="/css/sans.css">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Оберіть свій умкойн-гаманець</h1>
+    <p class="summary">Знайдіть гаманець та починайте здійснювати платежі з продавцями та користувачами.</p>
+
+    <div class="walletsdisclaimer">
+      <h2 id="educate-yourself">
+        <img src="/img/icons/ico_warning.svg" class="warningicon" alt="warning">
+        Приділіть достатньо часу, щоб усе вивчити
+      </h2>
+      <p>Умкойн відрізняється від того, що ви знаєте і використовуєте повсякденно. Перед тим як почати використовувати Умкойн для здійснення серйозних транзакцій, обов'язково прочитайте розділ <a href="/uk/you-need-to-know.php"><b>те, що вам потрібно знати</b></a> і вжийте необхідних заходів щодо <a href="/uk/secure-your-wallet.php"><b>захисту вашого гаманця</b></a>. Завжди пам'ятайте, що вибір гаманця та вжиття необхідних заходів щодо захисту власних грошей - це виключно ваша відповідальність.</p>
+    </div>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+<script type="text/javascript">
+  fallbackSVG();
+  addAnchorLinks();
+  trackOutgoingLinks();
+</script>
+
+
+</body>
+</html>
