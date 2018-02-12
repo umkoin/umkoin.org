cc9cfed2ad69ddf81b74e5be8fe20bba0b406286
Igor Gunia <vmta@yahoo.com>
2018-02-12 09:13:21 +0200

    Getting started page updated.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ en/getting-started.php
@@ -0,0 +1,132 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Getting started - Umkoin</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Getting started with Umkoin</h1>
+    <p class="summary">Using Umkoin to pay and get paid is easy and accessible to everyone.</p>
+
+    <div class="starttitle"><span>How to use Umkoin</span></div>
+
+    <div class="start">
+
+      <div>
+
+        <div>
+          <h2 id="inform"><span>1.</span> Inform yourself</h2>
+          <p>Umkoin is different than what you know and use every day. Before you start using Umkoin, there are a few things that you need to know in order to use it securely and avoid common pitfalls.</p>
+          <div><a href="/en/you-need-to-know.php">Read more</a></div>
+        </div>
+
+        <div>
+          <h2 id="choose"><span>2.</span> Choose your wallet</h2>
+          <p>You can bring a Umkoin wallet in your everyday life with your mobile or you can have a wallet only for online payments on your computer. In any case, choosing your wallet can be done in a minute.</p>
+          <div><a href="/en/choose-your-wallet.php">Choose your wallet</a></div>
+        </div>
+
+      </div>
+
+      <div>
+
+        <div>
+          <h2 id="get"><span>3.</span> Get Umkoin</h2>
+          <p>You can get Umkoin by accepting it as a payment for goods and services. There are also several ways you can buy Umkoin.</p>
+          <div><a href="/en/buy.php">Buy Umkoin</a></div>
+        </div>
+
+        <div>
+          <h2 id="spend"><span>4.</span> Spend Umkoin</h2>
+          <p>There is a growing number of services and merchants accepting Umkoin all over the world. You can use Umkoin to pay them and rate your experience to help honest businesses to gain more visibility.</p>
+          <div><a href="/en/spend-umkoin.php">Find merchants and products</a></div>
+        </div>
+
+      </div>
+
+    </div>
+
+    <div class="starttitle"><span>How to accept Umkoin</span></div>
+
+    <div class="start">
+
+      <div>
+
+        <div>
+          <h2 id="merchantinform"><span>1.</span> Inform yourself</h2>
+          <p>Umkoin does not require merchants to change their habits. However, Umkoin is different than what you know and use every day. Before you start using Umkoin, there are a few things that you need to know in order to use it securely and avoid common pitfalls.</p>
+          <div><a href="/en/you-need-to-know.php">Read more</a></div>
+        </div>
+
+        <div>
+          <h2 id="merchantservice"><span>2.</span> Processing payments</h2>
+          <p>You can process payments and invoices by yourself or you can use merchant services and deposit money in your local currency or umkoins. Most point of sales businesses use a tablet or a mobile phone to let customers pay with their mobile phones.</p>
+          <div><a href="https://en.bitcoin.it/wiki/How_to_accept_Bitcoin,_for_small_businesses#Merchant_Services">Find merchant services</a></div>
+        </div>
+
+      </div>
+
+      <div>
+
+        <div>
+          <h2 id="tax"><span>3.</span> Accounting and taxes</h2>
+          <p>Merchants often deposit and display prices in their local currency. In other cases, Umkoin works similarly to a foreign currency. To get appropriate guidance regarding tax compliance for your own jurisdiction, you should contact a qualified accountant.</p>
+          <div><a href="https://en.bitcoin.it/wiki/How_to_accept_Bitcoin,_for_small_businesses#Setting_Prices">Read more</a></div>
+        </div>
+
+        <div>
+          <h2 id="visibility"><span>4.</span> Gaining visibility</h2>
+          <p>There is a growing number of users searching for ways to spend their umkoins. You can submit your business in online directories to help them easily find you. You can also display the <a href="https://en.bitcoin.it/wiki/Promotional_graphics">Umkoin logo</a> on your website or your brick and mortar business.</p>
+          <div><a href="/en/spend-umkoin.php">Submit your business</a></div>
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+<script type="text/javascript">
+  fallbackSVG();
+  addAnchorLinks();
+  trackOutgoingLinks();
+</script>
+
+
+</body>
+</html>
