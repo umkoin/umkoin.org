e5a576bec189b31911d3da60e07ce86e28329404
Igor Gunia <vmta@yahoo.com>
2018-02-18 08:03:15 +0200

    Developer Glossary page updated.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ uk/developer-glossary.php
@@ -0,0 +1,122 @@
+<!DOCTYPE HTML>
+<html lang="uk">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Глосарій Розробника - Умкойн</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+<link rel="stylesheet" href="/css/sans.css">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs">
+      <a href="/uk/">Умкойн</a>
+      &gt;
+      <a href="/uk/developer-documentation">Документація Розробника</a>
+      &gt;
+      Глосарій
+  </div>
+
+  <div id="content" class="content">
+
+    <link rel="stylesheet" href="/css/jquery-ui.min.css">
+
+    <h1>Глосарій розробника Умкойн</h1>
+    <p class="summary"></p>
+
+    <!--Temporary disclaimer BEGIN-->
+    <p class="devdoc_translation_warning">
+      <b>BETA</b>: This version of the documentation may contain English (original) parts, if you wish to contribute to translating this document to your language, please use the appropriate links under the headings, or visit
+      <a href="https://github.com/umkoin/umkoin.org/docs/assisting-with-translation#translate">this link</a>
+    </p>
+    <!--Temporary disclaimer END-->
+
+    <div class="notice">
+      <p><span>Це технічний словник. Звичайним користувачам ми радимо використовувати <a href="/uk/vocabulary">сторінку словнику</a>.</span></p>
+    </div>
+
+    <h2 id="section">Номери</h2>
+    <!-- no subhead-links here -->
+
+    <h2 id="section-1">А</h2>
+    <!-- no subhead-links here -->
+
+    <ul class="wrapped_list">
+      <li><a href="/uk/glossary/51-percent-attack">Атака 51 відсотка</a></li>
+      <li><a href="/uk/glossary/51-percent-attack">Атака більшості</a></li>
+    </ul>
+    <p><br class="clear" /></p>
+
+    <h2 id="section-2">Б</h2>
+    <!-- no subhead-links here -->
+
+    <ul class="wrapped_list">
+      <li><a href="/uk/glossary/block-chain">Блок-чейн</a></li>
+    </ul>
+    <p><br class="clear" /></p>
+
+    <h2 id="section-3">Л</h2>
+    <!-- no subhead-links here -->
+
+    <ul class="wrapped_list">
+      <li><a href="/uk/glossary/block-chain">Ланцюг блоків</a></li>
+    </ul>
+    <p><br class="clear" /></p>
+
+    <h2 id="section-4">Н</h2>
+    <!-- no subhead-links here -->
+
+    <ul class="wrapped_list">
+      <li><a href="/uk/glossary/block-chain">Найкращий ланцюг блоків</a></li>
+    </ul>
+    <p><br class="clear" /></p>
+
+    <div class="notice">
+      <p><span>Не можете знайти, що шукали? <a href="https://github.com/umkoin/umkoin.org/issues/new?title=New%20glossary%20term%20suggestion:">Запропонуйте новий термін</a> для глосарія!</span></p>
+    </div>
+
+  </div>
+
+</div>
+
+
+<script type="text/javascript">
+  fallbackSVG();
+  addAnchorLinks();
+  trackOutgoingLinks();
+</script>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+<script src="/js/jquery/jquery-1.11.2.min.js"></script>
+<script src="/js/jquery/jquery-ui.min.js"></script>
+<script src="/js/devsearch.js"></script>
+<script>updateToc();</script>
+
+
+</body>
+</html>
