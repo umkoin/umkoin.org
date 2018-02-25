bf65ad219bed699912536a6c2feeeed7b0f582f7
Igor Gunia <vmta@yahoo.com>
2018-02-25 09:18:12 +0200

    Prepare Glossary bits (uk).
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ uk/glossary/breadcrumbs.php
@@ -0,0 +1,6 @@
+      <a href="/uk/">Умкойн</a>
+      &gt;
+      <a href="/uk/developer-documentation.php">Документація Розробника</a>
+      &gt;
+      <a href="/uk/developer-glossary,php">Глосарій</a>
+      &gt;
---
+++ uk/glossary/notice.php
@@ -0,0 +1,3 @@
+<div class="notice">
+  <p><span>Це тлумачення з <a href="/uk/developer-glossary.php">технічного глосарія</a>.</span></p>
+</div>
