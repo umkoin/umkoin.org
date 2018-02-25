41d85e9a106669aee2818888280e0847d90a173b
Igor Gunia <vmta@yahoo.com>
2018-02-25 09:19:01 +0200

    51 percent attack page (uk) update.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ uk/glossary/51-percent-attack.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html lang="uk">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Атака 51%, Атака більшості - Umkoin Glossary</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="stylesheet" href="/css/sans.css">
+<link rel="shortcut icon" href="/favicon.png">
+</head>
+
+
+<body>
+
+
+<?php
+include '../page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs">
+    <?php include 'breadcrumbs.php'; ?>
+    Атака 51 відсотка
+  </div>
+
+  <div id="content" class="content">
+
+    <h1>Атака 51%, Атака більшості</h1>
+    <?php include 'notice.php'; ?>
+
+    <h2 id="section">Визначення</h2>
+    <p>Здатність контролювати більшу частину хеш рейту мережі, щоб контролювати історію транзакцій та запобігати створенню нових транзакцій</p>
+
+    <h3 id="section-1">Синоніми</h3>
+    <ul>
+      <li>
+        <p>Атака 51 відсотка</p>
+      </li>
+      <li>
+        <p>Атака більшості</p>
+      </li>
+    </ul>
+
+    <h3 id="section-2">Посилання</h3>
+    <ul>
+      <li>
+        <p><a href="/en/developer-guide.php#term-51-attack">51% attack</a> — Umkoin.org Developer Guide</p>
+      </li>
+    </ul>
+
+  </div>
+
+</div>
+
+
+<?php
+include '../page_footer.php';
+?>
+
+
+</body>
+</html>
