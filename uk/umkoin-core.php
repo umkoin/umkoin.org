afdf62fc71bd16fc96967dc7db8c0fd04d87a2d9
Igor Gunia <vmta@yahoo.com>
2018-03-14 23:44:38 +0200

    Umkoin Core page (uk) added.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ uk/umkoin-core.php
@@ -0,0 +1,94 @@
+<!DOCTYPE HTML>
+<html lang="uk">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Умкойн Core</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="stylesheet" href="/css/sans.css">
+<link rel="shortcut icon" href="/favicon.png">
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <div class="one-column">
+
+      <h1>Умкойн Core</h1>
+      <p class="summary">Helping You Keep Umkoin Decentralized</p>
+
+      <div class="callout">
+        <p><a href="/uk/download.php">Завантажити Умкойн Core</a>
+        <img src="/img/os/windows.png" alt="Windows" />
+<!--        <img src="/img/os/mac.png" alt="Mac" /> //-->
+        <img src="/img/os/linux.png" alt="Linux" />
+        </p>
+      </div>
+
+      <p>Umkoin Core is programmed to decide which block chain contains valid transactions. The users of Umkoin Core only accept transactions for that block chain, making it the Umkoin block chain that everyone else wants to use</p>
+      <p>It is these users who <b>keep Umkoin decentralized.</b> They individually run their own Umkoin Core full nodes, and each of those full nodes separately follows the exact same rules to decide which block chain is valid.</p>
+      <p>There’s no voting or other corruptible process involved: there’s just individual software following identical rules—”math”—to evaluate identical blocks and coming to identical conclusions about which block chain is valid.</p>
+      <p>This shared agreement (called consensus) allows people like you to only accept valid umkoins, <b>enforcing Umkoin’s rules</b> against even the most powerful miners.</p>
+      <p>In addition to improving Umkoin’s decentralization, Umkoin Core users get <a href="/en/umkoin-core/features/validation.php">better security</a> for their umkoins, <a href="/en/umkoin-core/features/privacy.php">privacy features</a> not available in other wallets, a choice of <a href="/en/umkoin-core/features/user-interface.php">user interfaces</a> and several other powerful features.</p>
+
+      <p><br /></p>
+
+      <div class="two-column-list">
+        <ul class="fa-ul">
+          <li>
+            <p><span class="fa-li fa fa-download fa-2x"></span>
+            <b><a href="/uk/download.php">Download</a></b><br />Завантажити Умкойн Core 0.16.0</p>
+          </li>
+          <li>
+            <p><span class="fa-li fa fa-rocket fa-2x"></span>
+            <b><a href="/en/umkoin-core/features.php">Features</a></b><br />Discover what Umkoin Core offers</p>
+          </li>
+          <li>
+            <p><span class="fa-li fa fa-code-fork fa-2x"></span>
+            <b><a href="/en/umkoin-core/contribute.php">Contribute</a></b><br />Code, translations, and more</p>
+          </li>
+        </ul>
+      </div>
+
+      <p><br class="clear" /></p>
+
+      <h3>News</h3>
+      <ul>
+        <li>
+          <p>2018-01-22 - Umkoin Core version 0.16.0 released</p>
+        </li>
+        <li>
+          <p>2017-12-13 - Umkoin Core version 0.15.0 released</p>
+        </li>
+      </ul>
+
+    </div>
+
+  </div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+</body>
+</html>
