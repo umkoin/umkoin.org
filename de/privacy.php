b7918533807808794a08fdd9132d90bf6995c297
Igor Gunia <vmta@yahoo.com>
2018-03-27 16:25:42 +0300

    Privacy Policy page (de) added.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ de/privacy.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Privacy Policy - Umkoin</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Privacy Policy</h1>
+    <p>This page informs you of our policies regarding the collection, use and disclosure of personal information we receive from users of our site (<a href="http://umkoin.org">http://umkoin.org</a>). We use your personal information to better understand your usage of the site, and to collect traffic statistics.</p>
+    <p>By using the site, you agree to the collection and use of information in accordance with this policy.</p>
+
+    <h2 id="logdata">Log Data</h2>
+    <p>Like many site operators, we collect information that your browser sends whenever you visit our site ("Log Data"). This Log Data may include information such as your computer's Internet Protocol ("IP") address (with replaced last byte), browser type, browser version, the pages of our site that you visit, the time and date of your visit, the time spent on those pages and other statistics.</p>
+
+    <h2 id="cookies">Cookies</h2>
+    <p>Cookies are files with small amount of data, which may include an anonymous unique identifier. Cookies are sent to your browser from a web site and stored on your computer's hard drive. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent. However, if you do not accept cookies, you may not be able to use some portions of our site.</p>
+
+    <p>We use cookies for the following purposes:</p>
+    <ul>
+      <li>To keep track of whether you have pressed the "OK" button on the cookie disclaimer, so we don't bother you with the notification if you have.</li>
+      <li>Our Analytics software (Google Analytics) uses cookies to measure and better understand user-interactions on our Site. You can read more about how Google Analytics uses cookies <a href="https://developers.google.com/analytics/devguides/collection/analyticsjs/cookie-usage">here</a>.</li>
+    </ul>
+
+    <h2 id="analytics">Google Analytics</h2>
+    <p>We use a third-party JavaScript plug-in provided by Google called "Google Analytics" to provide us with useful traffic statistics and to better understand how you use our site. We do not have direct access to the information obtained from Google Analytics, but Google provides us with a summary through their dashboard.</p>
+    <p>We may share the information obtained from Google Analytics with business partners who are interested in advertising on our website. The information shared with these business partners will not contain any personally identifying information (Google does not provide us with direct access to the data and therefore we cannot see this information).</p>
+    <p>You can opt-out of having your information collected by Google Analytics by downloading the Google Analytics opt-out browser add-on provided by Google. This will prevent your information being used by Google Analytics. Doing this will not affect your ability to use our Site in any way. You can download the opt-out browser add-on <a href="https://tools.google.com/dlpage/gaoptout">here</a>.</p>
+
+    <h2 id="changes">Changes to this Privacy Policy</h2>
+    <p>We may update this privacy policy from time to time. We will notify you of any changes by posting the new privacy policy on the Site. You are advised to review this privacy policy periodically for any changes.</p>
+    <p>Privacy Policy (English edition) was last updated on <?php print(date("F d, Y", filemtime("privacy.php")));?>.</p>
+
+    <h2 id="contact">Contact Us</h2>
+    <p>If you have any questions about our privacy policy, or how your data is being collected and processed, please e-mail <a href="mailto:privacy@umkoin.org?Subject=Privacy">privacy@umkoin.org</a>.</p>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+<script type="text/javascript">
+  fallbackSVG();
+  addAnchorLinks();
+  trackOutgoingLinks();
+</script>
+
+
+</body>
+</html>
