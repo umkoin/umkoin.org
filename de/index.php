9984d130e829ffb884a09dc19c25bd7c87aa5551
Igor Gunia <vmta@yahoo.com>
2018-03-27 16:24:33 +0300

    Main page (de) added.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ de/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html lang="de">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="description" content="Umkoin ist ein innovatives Zahlungsnetzwerk und eine neue Form von Geld. Finden Sie alles was sie wissen müssen und legen Sie los mit Umkoin auf umkoin.org">
+
+<title>Umkoin - Open Source P2P Geld</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <p class="mainsummary">Umkoin ist ein innovatives Zahlungsnetzwerk und eine neue Art von Geld.</p>
+
+    <div class="mainlist">
+
+      <div>
+        <div>
+          <img src="/img/icons/main_ico_instant.svg" alt="Icon">
+          <div>Sofort-Peer-to-Peer<br>Transaktionen</div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <img src="/img/icons/main_ico_worldwide.svg" alt="Icon">
+          <div>Weltweite<br>Zahlungen</div>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <img src="/img/icons/ico_lock.svg" alt="Icon">
+          <div>Geringe bis keine<br>Transaktionskosten</div>
+        </div>
+      </div>
+
+    </div>
+
+    <p class="maindesc">Umkoin nutzt Peer-To-Peer-Technologie, um ohne zentrale Autorität auszukommen; die Bearbeitung von Transaktionen und die Ausgabe von Umkoins wird kollektiv durch das Netzwerk übernommen. <b>Umkoin ist Open-Source; das Design ist öffentlich, Umkoin gehört niemandem und wird von niemandem kontrolliert. <a href="/de/umkoin-unterstuetzen.php">Jeder kann teilhaben</a></b>. Durch viele seiner einzigartigen Eigenschaften eröffnet Umkoin aufregende Nutzungsmöglichkeiten, die durch keines der bisherigen Zahlungssysteme abgedeckt sind.</p>
+    <div class="mainbutton"><a href="/de/erste-schritte.php"><img src="/img/icons/but_umkoin.png" alt="icon">Legen Sie los mit Umkoin</a></div>
+    <div class="mainoverview">Oder bekommen Sie einen schnellen Überblick für</div>
+    <div class="mainoverviews"><a href="/de/umkoin-fuer-einzelpersonen.php">Einzelpersonen</a> <a href="/de/umkoin-fuer-unternehmen.php">Unternehmen</a> <a href="/de/umkoin-fuer-entwickler.php">Entwickler</a></div>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+</body>
+</html>
