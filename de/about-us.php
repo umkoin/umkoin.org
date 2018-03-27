85e15ad00e7a6c8f650fd9c9686ea18251904693
Igor Gunia <vmta@yahoo.com>
2018-03-27 16:25:02 +0300

    About Us page (de) added.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ de/about-us.php
@@ -0,0 +1,112 @@
+<!DOCTYPE HTML>
+<html lang="de">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<title>Über umkoin.org</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Über umkoin.org</h1>
+    <p class="summary">Umkoin.org ist einer nachhaltigen Entwicklung von Umkoin gewidmet.</p>
+
+    <h2 id="own">Wem gehört umkoin.org?</h2>
+    <p>Umkoin.org ist der ursprünglich für die erste Umkoin-Webseite benutzte Domainname. Registriert und verwaltet wurde/wird die Website durch die <a href="/de/development.php">Umkoin Hauptentwickler</a>. Umkoin.org ist keine offizielle Webseite. Genauso wie niemand die E-Mail-Technologie besitzt, besitzt auch niemand das Umkoin Netzwerk. Und somit kann niemand als Autorität im Namen von Umkoin sprechen.</p>
+
+    <h2 id="control">Dann... wer kontrolliert Umkoin?</h2>
+    <p>Umkoin wird durch alle Umkoin-Nutzer auf der ganzen Welt kontrolliert. Entwickler verbessern die Software, aber Sie können keine Änderung der Regeln im Umkoin-Protokoll erzwingen, da alle Nutzer die freie Wahl bei der Softwarenutzung haben. Um untereinander kompatibel zu bleiben, müssen alle Nutzer Software verwenden, die die selben Regeln befolgen. Umkoin kann nur bei einem vollständigen Konsens zwischen allen Benutzern ordentlich arbeiten. Daher haben alle Benutzer und Entwickler einen starken Anreiz diesen Konsens anzunehmen und zu schützen.</p>
+
+    <h2 id="mission">Auftrag</h2>
+    <ul>
+      <li>Benutzer informieren um Sie vor allgemeinen Fehlern zu schützen.</li>
+      <li>Eine genaue Beschreibung der Eigenschaften von Umkoin, potentielle Einsatzmöglichkeiten und Limitierungen geben.</li>
+      <li>Alarme und Ereignisse in Bezug auf das Umkoin Netzwerk anzeigen.</li>
+      <li>Talentierte Menschen einladen, um der Entwicklung von Umkoin auf vielen Ebenen zu helfen.</li>
+      <li>Einblick in das umfangreiche Umkoin-Ökosystem ermöglichen</li>
+      <li>Die weltweite Erreichbarkeit von Umkoin durch Internationalisierung verbessern.</li>
+      <li>Eine neutrale Informationsquelle über Umkoin bleiben.</li>
+    </ul>
+
+    <h2 id="help">Hilf uns</h2>
+    <p>Sie können jedes Problem durch Eröffnen eines "Issues" oder "Pull Requests" in englischer Sprache auf <a href="https://github.com/umkoin/umkoin.org#how-to-participate">GitHub</a> melden. Wenn Sie ein "Pull Request" übermitteln, nehmen Sie sich bitte die nötigte Zeit, um die Änderungen zu diskutieren und ihre Arbeit anzupassen. Bitte fragen Sie nicht nach Werbung für ihr eigenes Unternehmen oder ihre Website, außer in speziellen Fällen wie Konferenzen. Vielen Dank an alle Unterstützer, die ihre Zeit für die Verbesserung von umkoin.org aufwenden!</p>
+
+    <h3 id="maintenance">Wartung</h3>
+    <div class="credit">
+      <p><a href="https://github.com/vmta">vmta</a><span>Website maintainer</span></p>
+    </div>
+
+<!--
+    <h3 id="sponsorship">Sponsorship</h3>
+    <div class="credit">
+      <p><a href="">Organization</a><span>Period</span></p>
+    </div>
+//-->
+
+<!--
+    <h3 id="translation">Übersetzungen</h3>
+    <div class="credit">
+      <p>Translator<span>Language</span></p>
+    </div>
+//-->
+
+    <h3 id="service_contributors">Service Contributors</h3>
+    <div class="credit">
+      <p><a href="https://github.com/">GitHub</a><span>Repository hosting</span></p>
+      <p><a href="https://www.transifex.com/">Transifex</a><span>Translation tools</span></p>
+      <p><a href="https://travis-ci.org/">Travis CI</a><span>Continuous integration</span></p>
+    </div>
+
+<!--
+    <h3 id="inactive-contributors">Inactive Contributors</h3>
+    <div class="credit">
+      <p><a href="">Contributor</a><span>Project</span></p>
+    </div>
+//-->
+
+    <h3 id="owners">Domain Owners</h3>
+    <div class="credit">
+      <p><a href="https://github.com/vmta">vmta</a><span>Owner</span></p>
+    </div>
+
+    <h3 id="github">Unterstützer auf GitHub</h3>
+    <div class="contributors">
+      <p><a href="https://github.com/vmta">vmta</a></p>
+    </div>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+</body>
+</html>
