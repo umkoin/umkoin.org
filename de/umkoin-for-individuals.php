728e45a5b559d80bc58f3bb18121fc466c97eeab
Igor Gunia <vmta@yahoo.com>
2018-03-28 09:10:43 +0300

    Umkoin For Individuals page updated.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ de/umkoin-for-individuals.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html lang="de">
+
+
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<meta property="og:image" content="http://umkoin.org/img/icons/opengraph.png" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="description" content="Umkoin ist ein innovatives Zahlungsnetzwerk und eine neue Form von Geld. Finden Sie alles was sie wissen müssen und legen Sie los mit Umkoin auf umkoin.org.">
+
+<title>Umkoin für Einzelpersonen - Umkoin</title>
+
+<link rel="stylesheet" href="/css/font-awesome-4.4.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/css/main.css">
+<link rel="shortcut icon" href="/favicon.png">
+
+<script type="text/javascript" src="/js/base.js"></script>
+<script type="text/javascript" src="/js/main.js"></script>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Umkoin für Einzelpersonen</h1>
+    <p class="summary">Umkoin ist der einfachste Weg, Geld mit sehr geringen Kosten zu tauschen.</p>
+
+    <h2 id="mobile"><img class="titleicon" src="/img/icons/ico_mobile.svg" alt="Icon"/>Mobiles Bezahlen einfach gemacht</h2>
+    <p>Umkoin auf Mobilgeräten ermöglicht Ihnen das Zahlen in zwei einfachen Schritten: Scannen und Zahlen. Es muss keine Karte eingeführt, PIN eingegeben und nichts unterschrieben werden. Und um Umkoin-Zahlungen zu empfangen, müssen Sie nichts weiter tun als den QR-Code in Ihrer Umkoin Wallet-App anzuzeigen und Ihren Freund diesen scannen lassen, oder die beiden Telefone zusammen zu halten (um die NFC Technologie zu verwenden).</p>
+
+    <h2 id="secure"><img class="titleicon" src="/img/icons/ico_lock.svg" alt="Icon" />Sicherheit und Kontrolle über ihr Geld</h2>
+    <p>Umkoin-Transaktionen sind durch Kryptographie auf militärischem Niveau verschlüsselt. Niemand kann eine Zahlung in Ihrem Namen machen oder Geld einziehen. Solange Sie die notwendigen Schritte zur <a href="/de/secure-your-wallet.php">Absicherung Ihres Wallets</a> beachten, bietet Umkoin guten Schutz gegen viele Arten von Betrug.</p>
+
+    <h2 id="simple"><img class="titleicon" src="/img/icons/ico_simple.svg" alt="Icon" />Funktioniert immer und überall</h2>
+    <p>Genauso wie bei E-Mails müssen Sie ihre Familie nicht zwingen, die gleiche Software oder die gleichen E-Mail-Anbieter zu verwenden. Jeder kann bei seinem Favoriten bleiben. Dies stellt auch kein Problem dar, denn alle sind miteinander kompatibel, da diese die selbe Open-Source Technologie verwenden. Das Umkoin-Netzwerk schläft nie, auch nicht in den Ferien!</p>
+
+    <h2 id="international"><img class="titleicon" src="/img/icons/ico_international.svg" alt="Icon" />Schnelle internationale Zahlungen</h2>
+    <p>Umkoins können in 10 Minuten von Afrika nach Kanada transferiert werden. Es gibt keine Bank, die den Vorgang verlangsamt, überhöhte Gebühren verlangt oder den Transfer blockiert. Sie können einem Familienmitglied in einem anderen Land ebenso schnell Geld senden wie ihren Nachbarn.</p>
+
+    <h2 id="lowfee"><img class="titleicon" src="/img/icons/ico_lowfee.svg" alt="Icon" />Geringe bis keine Transaktionskosten</h2>
+    <p>Mit Umkoin können Sie zu sehr niedrigen Gebühren Zahlungen senden und empfangen. Abgesehen von Spezialfällen wie sehr kleinen Micro-Payments, gibt es keine erzwungene Gebühr. Es wird aber empfohlen eine kleine freiwillige Gebühr zu leisten, um die Priorität Ihrer Transaktion zu erhöhen und die Personen zu honorieren, die das Umkoin-Netzwerk betreiben.</p>
+
+    <h2 id="anonymous"><img class="titleicon" src="/img/icons/ico_anon.svg" alt="Icon" />Schützen Sie Ihre Identität</h2>
+    <p>Mit Umkoin gibt es keine Kreditkartennummer, die böswillige Aktoren sammeln könnten, um sich als Sie auszugeben. Tatsächlich ist es sogar wie mit echtem Geld möglich eine Zahlung zu senden ohne Ihre Identität preiszugeben. Sie sollten aber beachten, dass es etwas Aufwand benötigt um <a href="/de/protect-your-privacy.php">ihre Privatsphäre zu schützen</a>.</p>
+
+    <div class="mainbutton"><a href="/de/getting-started.php"><img src="/img/icons/but_umkoin.png" alt="icon">Legen Sie los mit Umkoin</a></div>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+</body>
+</html>
--- en/umkoin-for-individuals.php
+++ en/umkoin-for-individuals.php
@@ -66,12 +66,5 @@ include 'page_footer.php';
 ?>
 
 
-<script type="text/javascript">
-  fallbackSVG();
-  addAnchorLinks();
-  trackOutgoingLinks();
-</script>
-
-
 </body>
 </html>
