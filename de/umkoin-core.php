589988a26432104e2e7340954baa2b4cc5234e05
Igor Gunia <vmta@yahoo.com>
2018-03-31 01:10:00 +0300

    Umkoin Core and Vocabulary (de) pages added.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ de/umkoin-core.php
@@ -0,0 +1,88 @@
+<!DOCTYPE HTML>
+<html lang="de">
+
+
+<head>
+<?php include 'head'; ?>
+<title>Umkoin Core</title>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <div class="one-column">
+
+      <h1>Umkoin Core</h1>
+      <p class="summary">Hilft Ihnen, Umkoin Dezentral zu halten</p>
+
+      <div class="callout">
+        <p><a href="/en/download.php">Umkoin Core herunterladen</a>
+        <img src="/img/os/windows.png" alt="Windows" />
+<!--        <img src="/img/os/mac.png" alt="Mac" /> //-->
+        <img src="/img/os/linux.png" alt="Linux" />
+        </p>
+      </div>
+
+      <p>Umkoin Core ist programmiert, um zu entscheiden, welche Blockkette gültige Transaktionen enthält. Die Benutzer von Umkoin Core akzeptieren nur Transaktionen für diese Blockkette und bilden damit die Umkoin-Blockkette, die alle anderen verwenden möchten.</p>
+      <p>Es sind diese Benutzer, die <b>Umkoin dezentralisiert halten</b>. Sie führen einzeln ihre eigenen Umkoin Core-Vollknoten aus, und jeder dieser Vollknoten folgt separat exakt den gleichen Regeln, um zu entscheiden, welche Blockkette gültig ist.</p>
+      <p>Es gibt keine Abstimmung oder andere korrupte Prozesse: Es gibt nur eine individuelle Software, die nach identischen Regeln arbeitet - "Mathematik" - um identische Blöcke auszuwerten und zu identischen Schlussfolgerungen zu kommen, welche Blockkette gültig ist.</p>
+      <p>Diese gemeinsame Vereinbarung (Konsens genannt) erlaubt es Leuten wie Ihnen, nur gültige Umkoins zu akzeptieren und <b>Umkoins Regeln</b> sogar gegen die mächtigsten Bergleute durchzusetzen.</p>
+      <p>Zusätzlich zur Verbesserung der Umkoin-Dezentralisierung erhalten Umkoin-Core-Benutzer eine <a href="/en/umkoin-core/features/validation.php">bessere Sicherheit</a> für ihre Umkoins, <a href="/en/umkoin-core/features/privacy.php">Datenschutzfunktionen</a>, die in anderen Brieftaschen nicht verfügbar sind, eine Auswahl von Benutzerschnittstellen und mehrere andere leistungsstarke Funktionen.</p>
+
+      <p>In addition to improving Umkoin’s decentralization, Umkoin Core users get <a href="/en/umkoin-core/features/validation.php">better security</a> for their umkoins, <a href="/en/umkoin-core/features/privacy.php">privacy features</a> not available in other wallets, a choice of <a href="/en/umkoin-core/features/user-interface.php">user interfaces</a> and several other powerful features.</p>
+
+      <p><br /></p>
+
+      <div class="two-column-list">
+        <ul class="fa-ul">
+          <li>
+            <p><span class="fa-li fa fa-download fa-2x"></span>
+            <b><a href="/en/download.php">Herunterladen</a></b><br />Herunterladen Umkoin Core 0.16.0</p>
+          </li>
+          <li>
+            <p><span class="fa-li fa fa-rocket fa-2x"></span>
+            <b><a href="/en/umkoin-core/features.php">Eigenschaften</a></b><br />Entdecken Sie, was Umkoin Core bietet</p>
+          </li>
+          <li>
+            <p><span class="fa-li fa fa-code-fork fa-2x"></span>
+            <b><a href="/en/umkoin-core/contribute.php">Beitragen</a></b><br />Code, Übersetzungen und mehr</p>
+          </li>
+        </ul>
+      </div>
+
+      <p><br class="clear" /></p>
+
+      <h3>News</h3>
+      <ul>
+        <li>
+          <p>2018-01-22 - Umkoin Core version 0.16.0 veröffentlicht</p>
+        </li>
+        <li>
+          <p>2017-12-13 - Umkoin Core version 0.15.0 veröffentlicht</p>
+        </li>
+      </ul>
+
+    </div>
+
+  </div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+</body>
+</html>
---
+++ de/vocabulary.php
@@ -0,0 +1,99 @@
+<!DOCTYPE HTML>
+<html lang="de">
+
+
+<head>
+<?php include 'head'; ?>
+<title>Glossar - Umkoin</title>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Einige Wörter, die Sie im Zusammenhang mit Umkoin hören könnten</h1>
+    <p class="summarytxt">Umkoin geht neue Wege bei Zahlungen, daher gibt es einige neue Wörter, die Teil Ihres Wortschatzes werden könnten. Keine Sorge, selbst das gewöhnliche Fernsehen hat neue Wörter erfunden!</p>
+
+    <h2 id="table-of-contents">Inhalt</h2>
+    <div class="index">
+      <a href="#adresse">Adresse</a>
+      <a href="#bestaetigung">Bestätigung</a>
+      <a href="#bit">Bit</a>
+      <a href="#block">Block</a>
+      <a href="#blockkette">Blockkette</a>
+      <a href="#doppelausgabe">Doppelausgabe</a>
+      <a href="#hash-rate">Hash Rate</a>
+      <a href="#kryptographie">Kryptographie</a>
+      <a href="#mining">Mining</a>
+      <a href="#p2p">P2P</a>
+      <a href="#privater-schluessel">Privater Schlüssel</a>
+      <a href="#signatur">Signatur</a>
+      <a href="#umk">UMK</a>
+      <a href="#wallet">Wallet</a>
+    </div>
+
+    <h2 id="adresse">Adresse</h2>
+    <p>Eine Umkoin-Adresse ist <b>wie eine Haus- oder E-Mail-Adresse</b>. Es ist die einzige Information, die Sie weitergeben müssen, um Umkoin-Zahlungen zu empfangen. Ein wichtiger Unterschied ist aber, dass man jede Adresse nur für eine einzige Transaktion verwenden sollte.</p>
+
+    <h2 id="bestaetigung">Bestätigung</h2>
+    <p>Wenn eine Transaktion durch das Netzwerk bestätigt wurde, heißt das, dass sie <b>durch das Netzwerk verifiziert wurde und eine Rückabwicklung sehr unwahrscheinlich ist</b>. Transaktionen erhalten eine Bestätigung wenn sie in einem <a href="/de/glossar#block">Block</a> gespeichert sind, sowie in jedem darauffolgenden Block. Selbst eine Bestätigung kann für kleine Transaktionen als sicher betrachtet werden, wobei es bei größeren Geldmengen (z.B. 1000€ und mehr)  Sinn macht auf sechs oder mehr Bestätigungen zu warten. Jede neue Bestätigung reduziert das Risiko einer Rückabwicklung <i>exponentiell</i>.</p>
+
+    <h2 id="bit">Bit</h2>
+    <p>Bit ist eine häufig verwendete Einheit und stellt den Bruchteil eines ganzen Umkoins dar - 1.000.000 Bits entsprechen einem Umkoin. Bits sind häufig besser dafür geeignet, die Preise von Trinkgeld, Waren und Dienstleistungen anzugeben.</p>
+
+    <h2 id="block">Block</h2>
+    <p>Ein Block ist ein <b>Datensatz in der Blockkette, der viele ausstehende Transaktionen enthält und diese bestätigt</b>. Im Durchschnitt wird etwa alle 10 Minuten ein neuer Block inkl. der Transaktionen an die <a href="#blockkette">Blockkette</a> angehängt, dies geschieht durch <a href="#mining">Mining</a>.</p>
+
+    <h2 id="blockkette">Blockkette</h2>
+    <p>Die Blockkette ist eine <b>öffentliche Liste aller Umkoin-Transaktionen</b>, in chronologischer Reihenfolge. Alle Umkoin-Nutzer teilen die Blockkette. Sie wird benutzt um die Beständigkeit von Umkoin Transaktionen zu bestätigen und um <a href="#doppelausgabe">Doppelausgaben</a> zu verhindert.</p>
+
+    <h2 id="doppelausgabe">Doppelausgabe</h2>
+    <p>Wenn ein bösartiger Nutzer versucht <b>Umkoins gleichzeitig an zwei verschiedene Empfänger zu versenden</b>, wird von Doppelausgaben gesprochen. Umkoin <a href="#mining">Mining</a> und die <a href="#blockkette">Blockkette</a> sind dazu da um im Netzwerk ein Konsens zu erzeugen, welche der beiden Transaktionen bestätigt und als gültig angesehen wird.</p>
+
+    <h2 id="hash-rate">Hash Rate</h2>
+    <p>Die Hash Rate ist die <b>Maßeinheit der Rechenkraft des Umkoin-Netzwerks</b>. Aus Sicherheitsgründen muss das Umkoin-Netzwerk intensive mathematische Rechenoperationen durchführen. Wenn das Netzwerk eine Hash Rate von 10 TH/s erreicht, heißt das, dass es 10 Billionen Berechnungen pro Sekunde druchführen kann.</p>
+
+    <h2 id="kryptographie">Kryptographie</h2>
+    <p>Kryptographie ist ein Fachgebiet der Mathematik, mit dessen Hilfe <b>mathematische Beweise erstellt werden, die ein hohes Maß an Sicherheit bieten</b>. Der Online-Handel und Banken nutzen Kryptographie bereits. Im Falle von Umkoin wird Kryptographie verwendet, um auszuschließen, dass jemand Geld aus fremden Wallets ausgibt oder die <a href="#blockkette">Blockkette</a> manipuliert. Kryptographie kann auch verwendet werden, um eine Wallet zu verschlüsseln, so dass sie nicht ohne Passwort verwendet werden kann.</p>
+
+    <h2 id="mining">Mining</h2>
+    <p>Umkoin Mining ist die <b>die Durchführung mathematischer Berechnungen durch Computer Hardware, um Umkoin-Transaktionen zu bestätigen</b> und die Sicherheit zu erhöhen. Als Belohnung für Ihre Dienste können Umkoin-Miner Transaktionsgebühren für von ihnen bestätigte Transaktionen und neu erschaffene Umkoins sammeln. Mining ist ein spezialisierter und wettbewerbsgetriebener Markt, die Belohnungen werden je nach geleisteter Rechenarbeit aufgeteilt. Nicht alle Umkoin-Nutzer betreiben Mining, und es ist kein einfacher Weg, an Geld zu kommen.</p>
+
+    <h2 id="p2p">P2P</h2>
+    <p>Der Begriff "Peer To Peer" bezeichnet <b>Systeme, die wie ein organisiertes Kollektiv arbeiten</b> indem jedes Individuum direkt mit den anderen interagiert. Im Fall von Umkoin ist das Netzwerk so ausgelegt, dass jeder Nutzer die Transkationen anderer Nutzer übermittelt. Und, sehr wichtig: es wird keine Bank als dritte Instanz benötigt.</p>
+
+    <h2 id="privater-schluessel">Privater Schlüssel</h2>
+    <p>Ein privater Schlüssel ist ein <b>geheimer Datenblock, der Ihnen das Recht gewährt, Umkoins von einer spezifischen Umkoin Wallet auszugeben</b>, durch eine kryptographische <a href="#signatur">Signatur</a>.  Ihre privaten Schlüssel sind auf Ihrem Computer gespeichert, wenn Sie eine Software-Wallet verwenden oder sie sind auf entfernten Servern gespeichert, wenn Sie eine Web-Wallet verwenden. Private Schlüssel dürfen niemals weitergegeben werden, da mit ihnen die Umkoins der jeweiligen Wallets ausgegeben werden können.</p>
+
+    <h2 id="signatur">Signatur</h2>
+    <p>Eine <a href="#kryptographie">kryptographische</a> Signatur ist <b>ein mathematischer Mechanismus, um Eigentumsrecht nachzuweisen</b>. Im Fall von Umkoin, werden eine <a href="#wallet">Umkoin Wallet</a> und ihre <a href="#privater-schluessel">privaten Schlüssel</a> durch eine Art mathematische Magie miteinander verbunden. Wenn Ihre Umkoin-Software eine Transaktion mit dem passenden privaten Schlüssel signiert, kann das ganze Netzwerk sehen, dass die Signatur zu der Umkoin-Adresse passt. Dennoch kann niemand Ihren privaten Schlüssel erraten, um so Ihre hart verdienten Umkoins zu stehlen.</p>
+
+    <h2 id="umk">UMK</h2>
+    <p>Die Abkürzung UMK wird häufig als Einheit für Umkoin verwendet (1 UMK = 1 Umkoin).</p>
+
+    <h2 id="wallet">Wallet</h2>
+    <p>Eine Umkoin-Wallet ist <b>das Gegenstücke einer echten Geldbörse im Umkoin-Netzwerk</b>. Genau genommen enthält die Wallet Ihre <a href="#privater-schluessel">privaten Schlüssel</a>, die Ihnen das Recht geben, die Umkoins auszugeben, die in der <a href="#blockkette">Blockkette</a> ihr zugewiesen sind. Jede Umkoin-Wallet kann Ihnen den aktuellen Kontostand anzeigen und ermöglicht es Ihnen, eine bestimmte Geldmenge an bestimmte Personen zu zahlen, wie mit einer echten Geldbörse. Das macht den Unterschied zu Kreditkarten aus, bei denen das Geld vom Händler eingezogen wird.</p>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+</body>
+</html>
