df45325e1c4e84f8f8bc2578a47dc3c89c870de7
Igor Gunia <vmta@yahoo.com>
2018-04-01 00:39:35 +0300

    Secure Your Wallet (de) page added.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ de/secure-your-wallet.php
@@ -0,0 +1,111 @@
+<!DOCTYPE HTML>
+<html lang="de">
+
+
+<head>
+<?php include 'head'; ?>
+<title>Sichern Sie Ihre Wallet - Umkoin</title>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Sichern Sie Ihre Wallet</h1>
+    <p class="summarytxt">Wie im richtigen Leben, müssen Sie Ihre Wallet sicher verwahren. Umkoin macht den weltweiten Transfer von Werten einfacher als jemals zuvor. Bei so großartigen Möglichkeiten gehören große Sicherheitsbedenken grundsätzlich dazu. Richtig eingesetzt, kann Umkoin sehr hohe Sicherheitsansprüche erfüllen. <b>Bedenken Sie immer, dass es in Ihrer Verantwortung liegt, Ihr Geld so gut wie möglich abzusichern</b>.</p>
+
+    <h2 id="online"><img src="/img/icons/ico_warning.svg" class="titleicon" alt="warning">Seien Sie vorsichtig mit Online-Services</h2>
+    <p>Sie sollten vorsichtig mit Services umgehen, die dazu entworfen sind Geld online aufzubewahren. Viele Wechselstuben und Online Wallets litten in jüngster Zeit unter Sicherheitslücken und allgemein bieten solche Dienste nicht genug Sicherheit, um dort sein Geld zu lagern wie bei einer Bank. Von daher sollten Sie andere Arten von <a href="/de/choose-your-wallet.php">Umkoin Wallets</a> verwenden. Andernfalls sollen sie Online Services sehr sorgfältig auswählen. Zusätzlich wird empfohlen, Zwei-Faktor Authentifizierung zu verwenden.</p>
+
+    <h2 id="everyday">Geringe Mengen für den täglichen Bedarf</h2>
+    <p>Eine Umkoin-Wallet is wie eine Geldbörse mit Bargeld. Falls Sie nicht mehrere tausend Euro in ihrem Geldbeutel aufbewahren würden, sollten Sie das auch bei ihrer Umkoin-Wallet in Betracht ziehen. Überhaupt ist es ein bewährtes Verfahren nur kleine Mengen von Umkoins für den täglichen Bedarf auf ihrem Computer oder Smartphone zu haben und den restlichen Teil ihres Vermögens an einem sicheren Ort zu verwahren.</p>
+
+    <h2 id="backup">Machen Sie Sicherheitskopien Ihrer Wallet</h2>
+    <p>Ein Backup ihrer Wallet, verwahrt an einem sicheren Ort, kann Sie vor Verlust durch Computerdefekte und menschliche Fehler schützen. Ein Backup ermöglicht es Ihnen aber auch ihre Wallet wiederherzustellen, nachdem ihr Smartphone oder Computer gestohlen wurde, falls Sie ihr Wallet verschlüsselt haben.</p>
+
+    <div class="box">
+
+      <h3 id="backupwhole">Legen Sie Sicherheitskopien Ihrer kompletten Wallet an</h3>
+      <p>Manche Wallets verwenden zahlreiche verborgene private Schlüssel. Wenn Sie nur eine Sicherheitskopie für ihre sichtbaren Umkoin-Adressen haben, kann es passieren, dass Sie einen großen Teil Ihrer Gelder nicht wiederherstellen können.</p>
+
+      <h3 id="backuponline">Verschlüsseln Sie Online-Sicherheitskopien</h3>
+      <p>Das Diebstahlrisiko einer online gespeicherten SIcherheitskopie ist sehr hoch. Auch ein an das Internet angeschlossener Computer kann von bösartiger Software befallen werden. Daher empfiehlt es sich, Sicherheitskopien zu verschlüsseln, wenn Sie über ein Netzwerk erreichbar sein könnten.</p>
+
+      <h3 id="backupmany">Nutzen Sie verschiedene sichere Speicherorte</h3>
+      <p>Einzelne Schwachstellen sind schlecht für die Sicherheit. Wenn Ihre Sicherheitskopie nicht von einem einzigen Speicherort abhängt, ist es unwahrscheinlicher, dass ein negatives Ereignis dazu führt, dass Sie Ihre Wallet nicht wiederherstellen können. Ziehen Sie in Betracht verschiedenen Medien einzusetzen, z.B. USB-Sticks, Papier und CDs.</p>
+
+      <h3 id="backupregular">Fertigen Sie regelmäßig Sicherheitskopien an</h3>
+      <p>Sie sollten regelmäßig Sicherheitskopien Ihrer Wallet anlegen, um sicher zu stellen, dass alle kürzlich erstellen Umkoin-Adresse darin enthalten sind. Bald werden aber alle Programme Wallets nutzen, die nur einmal gesichert werden müssen.</p>
+
+    </div>
+
+    <h2 id="encrypt">Verschlüsseln Sie Ihr Wallet</h2>
+    <p>Wenn Sie Ihre Wallet oder ihr Smartphone verschlüsseln, können Sie ein Passwort festlegen, welches benötigt wird um Geld daraus zu verwenden. Diese Maßnahme hilft beim Schutz vor Dieben, allerdings bietet sie keinen Schutz gegen Keylogging-Hardware oder -Software.</p>
+
+    <div class="box">
+
+      <h3 id="encryptforget">Vergessen Sie niemals Ihr Passwort</h3>
+      <p>Sie sollten sicher stellen, dass Sie Ihr Passwort niemals vergessen, ansonsten ist Ihr Geld dauerhaft verloren. Im Gegensatz zu Ihrer Bank gibt es bei Umkoin sehr wenige Möglichkeiten, ein vergessenenes Passwort zurückzusetzen. Es sollte Ihnen sogar möglich sein, sich an Ihr Passwort zu erinnern, auch wenn Sie es mehrere Jahre lang nicht benutzt haben. Im Zweifelsfall können Sie in Betracht ziehen, Ihr Passwort auf Papier an einem sicheren Ort aufzubewahren, z.B. in einem Tresor.</p>
+
+      <h3 id="encryptstrong">Verwenden Sie ein sicheres Passwort</h3>
+      <p>Ein Passwort, das nur aus Buchstaben oder erkennbaren Wörtern besteht, gilt als sehr unsicher und einfach zu knacken. Ein sicheres Passwort muss Zahlen, Ziffern sowie Sonderzeichen enthalten und mindestens 16 Zeichen lang sein. Die sichersten Passwörter sind jene, die von Programmen generiert werden, die eigens für diese Aufgabe entworfen worden sind. In der Regel ist es schwieriger sich an sichere Passwörter zu erinnern, weshalb man sie sich ins Gedächnis einprägen sollte.</p>
+
+    </div>
+
+    <h2 id="offline">Offline-Wallet für Erspartes</h2>
+    <p>Eine Offline-Wallet, auch "Cold Storage" genannt, bietet das höchste Maß an Sicherheit für Erspartes. Hierbei wird eine Wallet an einem gesicherten Ort gespeichert, der keine Verbindung zum Internet hat. Wenn es ordentlich umgesetzt ist, bietet es einen sehr guten Schutz gegen Computerschwachstellen. Es empfiehlt sich auch, Offline-Wallets in Verbindung mit Sicherheitskopien und Verschlüsselung zu verwenden. Im Folgenden eine Übersicht über einige Ansätze.</p>
+
+    <div class="box boxexpand">
+
+      <h3 id="offlinetx"><a onclick="boxShow(event);" ontouchstart="boxShow(event);" class="link-js">Offline-Signierung von Transaktionen</a></h3>
+      <p>Bei diesem Ansatz teilen sich zwei Computer Teile einer Wallet. Der Erste darf nicht mit einem Netzwerk verbunden sein. Nur dieser speichert die komplette Wallet und kann Transaktionen signieren. Der zweite Computer ist mit dem Netzwerk verbunden, hat nur eine beobachtende Wallet und kann nur unsignierte Transaktionen erstellen. So können Sie mit den folgenden Schritten sicher neue Transaktionen veranlassen.</p>
+      <ol>
+        <li>Erstellen Sie eine neue Transaktion auf dem Online-Computer und speichern Sie sie auf einem USB-Stick.</li>
+        <li>Signieren Sie die Transaktion mit dem Offline-Computer.</li>
+        <li>Senden Sie die signierte Transaktion mit dem Online-Computer.</li>
+      </ol>
+      <p>Da der mit dem Netzwerk verbundene Computer keine Transaktionen signieren kann, kann er im Falle eines Angriffs nicht verwenden werden, um Geld zu transferieren. Zur Offline-Erstellung von Transaktionssignaturen kann <a href="https://bitcoinarmory.com/tutorials/armory-advanced-features/offline-wallets/">Armory</a> verwendet werden.</p>
+
+    </div>
+
+    <br>
+
+    <div class="box boxexpand">
+
+      <h3 id="hardwarewallet"><a onclick="boxShow(event);" ontouchstart="boxShow(event);" class="link-js">Hardware-Wallets</a></h3>
+      <p>Hardware-Wallets bieten den besten Kompromiss aus sehr hoher Sicherheit und einfacher Handhabung. Es sind kleine Geräte, die von Grundauf dafür entworfen wurden nur eine Wallet und sonst nichts weiter zu sein. Keine Software kann auf ihnen installiert werden, was sie sehr sicher gegen Computerangriffe und Onlinediebe macht. Weil sie eine Sicherung ermöglichen, können Sie ihr Geld aus einem verlorenem Gerät wiederherstellen.</p>
+
+    </div>
+
+    <h2 id="update">Halten Sie ihre Software auf dem neusten Stand.</h2>
+    <p>Die neuste Version ihrer Umkoin-Software zu verwenden, ermöglicht es ihnen, wichtige Stabilitäts- und Sicherheitsupdates zu erhalten. Updates können Probleme unterschiedlichen Ausmaßes beheben, beinhalten neue nützliche Funktionen und helfen dabei, ihre Wallet sicher zu verwahren. Das Installieren von Updates für sämtliche andere Software auf ihrem Computer oder Smartphone ist ebenfalls wichtig um die Wallet-Umgebung sicher zu halten.</p>
+
+    <h2 id="offlinemulti">Multi-Signatur zum Schutz vor Diebstahl</h2>
+    <p>Umkoin bietet eine Funktion namens Multi-Signatur, bei der die Signatur von mehr als einem privaten Schlüssel benötigt wird, um eine Transaktion zu tätigen. Eine Organisation kann ihren Mitgliedern über die Multi-Signatur Zugriff auf ihr Vermögen geben und gleichzeitig Ausgaben nur zulassen, wenn 3 von 5 Mitgliedern die Transaktion signieren. Einige Web-Wallets bieten auch Multi-Signatur Wallets, was dem Nutzer die Möglichkeit gibt die Kontrolle über sein Geld zu behalten und gleichzeitig den Diebstahl von selbigen durch ein einzelnes kompromittiertes Gerät oder Server zu verhinden.</p>
+
+    <h2 id="offlinetestament">Denken Sie an Ihr Testament</h2>
+    <p>Ihre Umkoins können unwiederbringlich verloren sein, wenn Sie keinen Sicherungsplan für Familie und Freunde haben. Wenn zum Zeitpunkt Ihres Todes der Speicherort Ihrer Wallets oder Ihre Passwörter nicht bekannt ist, gibt es keine Hoffnung, dass Ihr Geld jemals wiederhergestellt werden kann. Nehmen Sie sich ein wenig Zeit für diese Dinge, es kann einen großen Unterschied machen.</p>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+</body>
+</html>
