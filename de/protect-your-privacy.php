b4c431a1887799fd37de9a99fc5b9660d020a770
Igor Gunia <vmta@yahoo.com>
2018-04-02 00:27:01 +0300

    Protect Your Privacy page updated.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ de/protect-your-privacy.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<html lang="de">
+
+
+<head>
+<?php include 'head'; ?>
+<title>Schützen Sie ihre Privatsphäre - Umkoin</title>
+</head>
+
+
+<body>
+
+
+<?php
+include 'page_head.php';
+?>
+
+
+<div class="body">
+
+  <div class="breadcrumbs"></div>
+
+  <div id="content" class="content">
+
+    <h1>Schützen Sie ihre Privatsphäre</h1>
+    <p class="summarytxt">Umkoin wird häufig als anonymes Zahlungsnetzwerk verstanden. In Wirklichkeit ist Umkoin das vermutlich transparenteste Zahlungsnetzwerk der Welt. Gleichzeitig bietet Umkoin, richtig genutzt, einen annehmbaren Grad an Privatsphäre. <b>Denken Sie aber immer daran, dass es in ihrer Verantwortung liegt sich Methoden anzugewöhnen, mit denen Sie ihre Privatsphäre schützen können.</b></p>
+
+    <h2 id="traceable">Die Rückverfolgbarkeit von Umkoin verstehen</h2>
+    <p>Umkoin arbeitet mit einem noch nie dagewesenen Maß an Transparenz, mit dessen Umgang die  meisten Menschen nicht vertraut sind. Alle Umkoin-Transaktionen sind öffentlich, zurückverfolgbar und dauerhaft im Umkoin-Netzwerk gespeichert. Umkoin-Adressen sind die einzige Information, die verwendet wird um zu bestimmen, wie Umkoins zugeteilt sind bzw. wohin sie gesendet werden. Dieses Adressen werden von der Wallet jedes Nutzers privat generiert. Sobald jedoch Adressen verwendet werden, haftet die Historie aller Transaktionen, in die sie involviert sind, an ihnen. Jeder kann den  <a href="http://www.umkoin.org/en/blockexplorer.php">Kontostand und alle Transaktionen</a>  einer Adresse einsehen. Da Nutzer normalerweise ihre Identität preisgeben müssen, um Dienstleistungen oder Waren entgegenzunehmen, können Umkoin Adressen nicht vollständig anonym bleiben. Aus diesen Gründen sollten Umkoin Adressen nur einmalig verwendet werden, und Nutzer müssen vorsichtig sein um ihre Adressen nicht preiszugeben.</p>
+
+    <h2 id="receive">Benutzern Sie neue Adressen um Zahlungen zu empfangen</h2>
+    <p>Um ihre Privatsphäre zu schützen, sollten Sie jedes Mal, wenn Sie eine Zahlung empfangen, eine neue Adresse nutzen. Außerdem können Sie mehrere Wallets für unterschiedliche Zwecke nutzen. Auf diese Art können Sie jede ihrer Transaktionen trennen und es ist nicht möglich, sie alle miteinander in Verbindung zu bringen. Menschen, die ihnen Geld senden, können nicht sehen, welche anderen Umkoin-Adressen Ihnen gehören und was Sie damit machen. Das ist vermutlich eine der wichtigsten Ratschläge, die Sie beachten sollten.</p>
+
+    <h2 id="public">Seien Sie in öffentlichen Netzwerken vorsichtig</h2>
+    <p>So lange es nicht ihre Absicht ist, öffentliche Spenden oder Zahlungen mit vollkommener Transparenz zu empfangen, ist es im Sinne der Privatsphäre keine gute Idee eine Umkoin-Adresse auf ihrer Webseite oder in öffentlichen Netzwerken zu veröffentlichen. Wenn Sie sich trotzdem dazu entschließen, denken Sie immer daran, dass, wenn Sie Beträge von dieser Adresse auf eine ihrer anderen Adressen übertragen, beide Adressen in der Historie ihrer öffentlichen Adresse auftauchen. Zusätzlich sollten Sie darauf achten, keine Informationen über Transaktionen und Einkäufe zu veröffentlichen, die es jemanden ermöglichen, ihre Umkoin-Adressen zu identifizieren.</p>
+
+    <h2 id="iplog">Ihre IP-Adresse kann geloggt werden</h2>
+    <p>Weil Umkoin ein Peer-to-Peer Netzwerk ist, ist es möglich die Übermittlung von Transaktion zu überwachen und IP-Adressen aufzuzeichnen. "Full Node"-Clients übermitteln alle Transaktionen, als wären es ihre eigenen. Das bedeutet, dass das Auffinden der Quelle einer bestimmten Transaktion schwierig sein kann und jeder Umkoin-"Node" fälschlicherweise für den Ausgangspunkt gehalten werden kann. Sie sollten es in Betracht ziehen, die IP-Adresse ihres Computers mit einem Hilfsmittel wie <a href="https://www.torproject.org/">Tor</a> zu verstecken, so dass diese nicht aufgezeichnet werden kann.</p>
+
+    <h2 id="mixing">Einschränkungen von "Mixingservices"</h2>
+    <p>Einige "Mixingservices" genannte Onlinedienste bieten an, die Rückverfolgbarkeit von Adressen zwischen Nutzern zu erschweren, indem sie denselben Betrag mit unterschiedlichen Umkoin-Adressen empfangen und weiter versenden. Es ist wichtig zu wissen, dass die Legalität der Nutzung solcher Dienste je nach Gesetzgebung eines Landes unterschiedlichen Regeln unterworfen sein kann. Außerdem müssen Sie darauf vertrauen, dass die Personen, die diese Dienste anbieten, ihr Geld nicht verlieren, stehlen oder eine Logdatei ihrere Anfragen speichern. Auch wenn "Mixingservices" für kleine Beträge eine gute Möglichkeit bieten, die Rückverfolgbarkeit zu erschweren, wird es bei größeren Beträgen umso schwieriger.</p>
+
+    <h2 id="future">Zukünftige Verbesserungen</h2>
+    <p>Zukünftig können viele Verbesserungen, die die Privatsphäre erhöhen, erwartet werden. Zum Beispiel gibt es einige Bemühungen, um zu verhindern, dass mehrere Adressen durch eine Zahlung mitenander in Verbindung gebracht werden können. Umkoin Cores "Change Address" könnte mit der Zeit auch in andere Wallets implementiert werden. Grafische Nutzeroberflächen könnten verbessert werden, um nutzerfreundliche Zahlungsanfrage-Features zu bieten und um das Wiederverwenden von Adressen zu vermeiden. Vielerlei Arbeits- und Forschungstätigkeiten finden ebenso im Bereich der möglichen "Extended Privacy"-Features, wie die Möglichkeit zufällige Nutzertransaktionen zusammenzufügen, statt.</p>
+
+  </div>
+
+</div>
+
+
+<?php
+include 'page_footer.php';
+?>
+
+
+</body>
+</html>
