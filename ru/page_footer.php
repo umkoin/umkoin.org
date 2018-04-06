e55c841f14ff914c93cdbcaabe498ceda1abae26
Igor Gunia <vmta@yahoo.com>
2018-04-06 10:13:05 +0300

    Main page, About Us, Legal, Privacy Policy (ru) pages added.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

---
+++ ru/page_footer.php
@@ -0,0 +1,15 @@
+<div class="footer">
+
+  <div>
+    <p>Umkoin.org is community supported: <a href="umkoin:1FeDNQk5FuNCxJK7un4NhW8hRjpSC99g5t" target="_blank">1FeDNQk5FuNCxJK7un4NhW8hRjpSC99g5t</a></p>
+  </div>
+
+  <div class="footermenu">
+    <a href="/ru/legal.php">Юридические вопросы</a>
+    <a href="/ru/privacy.php">Политика кофиденциальности</a>
+    <a href="/ru/about-us.php">Об umkoin.org</a>
+  </div>
+
+  <div class="footerlicense">&copy; Umkoin Project 2017-<?php print(date("Y")); ?> ПО распространяется под <a href="http://opensource.org/licenses/mit-license.php" target="_blank">лицензией MIT</a></div>
+
+</div>
