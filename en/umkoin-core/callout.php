06d1655e4bda3787ee47ee7fbb8195eced9934e4
Igor Gunia <vmta@yahoo.com>
2018-11-16 22:12:26 +0200

    Add Apple logo.
    Signed-off-by: Igor Gunia <vmta@yahoo.com>

--- en/umkoin-core/callout.php
+++ en/umkoin-core/callout.php
@@ -1,6 +1,7 @@
       <div class="callout">
         <p><a href="/en/download">Download Umkoin Core</a>
         <img src="/img/os/windows.png" alt="Windows" />
+        <img src="/img/os/mac.png" alt="Mac" />
         <img src="/img/os/linux.png" alt="Linux" />
         </p>
       </div>
